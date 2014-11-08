11a0c161c59a61254d9a9884d9ee08cbb5e2df6d
hopeshpi <ndzhd.job@gmail.com>
2014-11-08 23:20:50 +0400

    Group tests compare

--- addressbook-selenium-tests/src/com/example/tests/GroupCreationTests.java
+++ addressbook-selenium-tests/src/com/example/tests/GroupCreationTests.java
@@ -1,6 +1,8 @@
 package com.example.tests;
 
+import java.util.Collections;
 import java.util.List;
+
 import static org.testng.Assert.assertEquals;
 
 import org.testng.annotations.Test;
@@ -29,19 +31,33 @@ public class GroupCreationTests extends TestBase {
 	  List<GroupData> newList = app.getGroupHelper().getGroups();
 
 	  // compare states
-	  assertEquals(newList.size(), oldList.size() + 1);
-	  
-	  
-	  
+	  oldList.add(group);
+	  Collections.sort(oldList);
+	  assertEquals(newList, oldList);	  
   	}
   
-    //@Test
+    @Test
 	public void testEmptyGroupCreation() throws Exception {
 	  app.getNavigationHelper().openMainPage();
 	  app.getNavigationHelper().gotoGroupsPage();
+	  
+	  // save old state
+	  List<GroupData> oldList = app.getGroupHelper().getGroups();
+	  
+	  //actions
 	  app.getGroupHelper().initGroupCreation();
-	  app.getGroupHelper().fillGroupForm(new GroupData("", "", ""));
+	  GroupData group = new GroupData("", "", "");
+	  app.getGroupHelper().fillGroupForm(group);
 	  app.getGroupHelper().submitGroupCreation();
 	  app.getGroupHelper().returnToGroupPage();
+	  
+	  // save new state
+	  List<GroupData> newList = app.getGroupHelper().getGroups();
+
+	  // compare states
+	  oldList.add(group);
+	  Collections.sort(oldList);
+	  assertEquals(newList, oldList); 
 	}
+    
 }
