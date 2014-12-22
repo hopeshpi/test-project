28973305743b56cabcedf598f950470a3ff44d8e
hopeshpi <ndzhd.job@gmail.com>
2014-12-22 19:21:10 +0300

    config for run TestNG
    + .bat

---
+++ addressbook-selenium-tests/run-testng-customsuite.bat
@@ -0,0 +1,3 @@
+@echo off
+
+java -cp bin;C:\dev\xstream-1.4.7\lib\xstream-1.4.7.jar;C:\dev\xstream-1.4.7\lib\xstream\xmlpull-1.1.3.1.jar;C:\dev\xstream-1.4.7\lib\xstream\xpp3_min-1.1.4c.jar;C:\dev\selenium-server-standalone-2.44.0.jar org.testng.TestNG testng-customsuite.xml
